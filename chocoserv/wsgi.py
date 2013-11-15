68ae2265daf86d485221d938947824a7871c6835
Rick Macedo <rick.macedo@gmail.com>
2013-11-15 16:36:22 -0200

    Views implementadas

--- chocoserv/wsgi.py
+++ chocoserv/wsgi.py
@@ -24,10 +24,13 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chocoserv.settings")
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
-from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+# from django.core.wsgi import get_wsgi_application
+# from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+# application = get_wsgi_application()
+
+import django.core.handlers.wsgi
+application = django.core.handlers.wsgi.WSGIHandler()
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
